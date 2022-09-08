9a71400906b8b4be0ab0a4a85127fbebd57d8c1b
saeyoung <syoungbb25@gmail.com>
2022-09-08 16:07:47 +0900

    useEffect 사용,React context 및 컴포넌트 내부에 context 사용하기

---
+++ src/store/favorites-context.js
@@ -0,0 +1,48 @@
+import { createContext, useState } from "react";
+
+const FavoritesContext = createContext({
+  favorites: [],
+  totalFavorites: 0,
+  addFavorite: (favoriteMeetup) => {},
+  removeFavorite: (meetupId) => {},
+  itemIsFavorite: (meetupId) => {},
+});
+
+export function FavoritesContextProvider(props) {
+  const [userFavorites, setUserFavorites] = useState([]);
+
+  //즐겨찾기 추가
+  function addFavoriteHandler(favoriteMeetup) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.concat(favoriteMeetup);
+    });
+  }
+
+  // 즐겨찾기 제거
+  function removeFavoriteHandler(meetupId) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.filter((meetup) => meetup.id !== meetupId);
+    });
+  }
+
+  function itemIsFavoriteHandler(meetupId) {
+    return userFavorites.some((meetup) => meetup.id == meetupId);
+  }
+
+  const context = {
+    favorites: userFavorites,
+    totalFavorites: userFavorites.length,
+    addFavorite: addFavoriteHandler,
+    removeFavorite: removeFavoriteHandler,
+    itemIsFavorite: itemIsFavoriteHandler,
+  };
+
+  return (
+    <FavoritesContext.Provider value={context}>
+      {/* 이 콘텍스트와 상호 작용하고자 하는 모든 컴포넌트를 둘러 싸고 래핑되어야한다. */}
+      {props.children}
+    </FavoritesContext.Provider>
+  );
+}
+
+export default FavoritesContext;
